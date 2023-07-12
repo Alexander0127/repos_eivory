57c799d764053d21383e364378e4bc852e86ab11
Alexander0127 <AlexanderTR0127@gmail.com>
2023-07-11 21:55:19 -0500

    num

--- php/index.php
+++ php/index.php
@@ -9,7 +9,7 @@
     <form method="post">
     	<h1>Conexion</h1>
     	<input type="text" name="name" placeholder="Nombre completo">
-    	<input type="text" name="edad" placeholder="Edad">
+    	<input type="number" name="edad" placeholder="Edad">
     	<input type="submit" name="register">
     </form>
         <?php 
