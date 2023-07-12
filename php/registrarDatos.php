5435dd6742067396e011ae50cdb02be60d383db0
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-11 22:13:47 -0500

    Update registrarDatos.php

--- php/registrarDatos.php
+++ php/registrarDatos.php
@@ -1,16 +1,19 @@
 include("conexion.php");
-
-if (isset($_POST['register'])) {
-    if (strlen($_POST['name']) >= 1 && strlen($_POST['edad']) >= 1) {
+if (isset($_POST['register'])) 
+{
+    if (strlen($_POST['name']) >= 1 && strlen($_POST['edad']) >= 1) 
+    {
 	    $name = trim($_POST['name']);
 	    $edad = trim($_POST['edad']);
 	    $consulta = "INSERT INTO usuario(nombre, edad) VALUES ('$name','$edad')";
 	    $resultado = mysqli_query($conex,$consulta);
-	    if ($resultado) {
+	    if ($resultado) 
+    {
 	    	?> 
 	    	<h3 class="ok">¡Te has inscripto correctamente!</h3>
            <?php
-	    } else {
+	    } 
+        else {
 	    	?> 
 	    	<h3 class="bad">¡Ups ha ocurrido un error!</h3>
            <?php
