c6a13576eb916bfbb5e4799358bde3827d2423ec
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-12 22:49:38 -0500

    Create registerInput.php

---
+++ php_registro/registerInput.php
@@ -0,0 +1,25 @@
+<?php
+include("conexion.php");
+
+if (isset($_POST['register'])) {
+    if (strlen($_POST['name']) >= 1 && strlen($_POST['edad']) >= 1) {
+	    $name = trim($_POST['name']);
+	    $edad = trim($_POST['edad']);
+	    $consulta = "INSERT INTO usuario(nombre, edad) VALUES ('$name','$edad')";
+	    $resultado = mysqli_query($conex,$consulta);
+	    if ($resultado) 
+	    	{?>
+	    	<h3 class="ok">¡Te has inscripto correctamente!</h3>
+            <?php
+	    } else {
+	    	?> 
+	    	<h3 class="bad">¡Ups ha ocurrido un error!</h3>
+           <?php
+	    }
+    }   else {
+	    	?> 
+	    	<h3 class="bad">¡Por favor complete los campos!</h3>
+           <?php
+    }
+}
+?>
