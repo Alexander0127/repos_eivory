b61da7d0494b69eabfe38026cdefc685ce1952b6
Alexander0127 <AlexanderTR0127@gmail.com>
2023-07-12 22:47:40 -0500

    inputs

--- php_registro/registro.php
+++ php_registro/registro.php
@@ -8,8 +8,12 @@
 <body>
     <form method="post">
     	<h1>Conexion</h1>
-    	<input type="text" name="name" placeholder="Nombre completo">
-    	<input type="number" name="edad" placeholder="Edad">
+    	<input type="text" name="name" placeholder="Nombres">
+		<input type="text" name="lastname" placehorder="Apellidos">
+    	<input type="number" name="phone" placeholder="Número Celular">
+		<input type="text" name="addres" placeholder="Dirección de entrega">
+		<input type="text" name="email" palceholder="Correo Electrónico">
+		<input type="text" name="password" placeholder="Contraseña">
     	<input type="submit" name="register">
     </form>
         <?php 
