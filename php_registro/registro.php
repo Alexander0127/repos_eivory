0563f2f75a461b6e864cec95080d12216aed0c22
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-12 22:38:12 -0500

    Update registro.php

--- php_registro/registro.php
+++ php_registro/registro.php
@@ -1,1 +1,19 @@
-asd
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registrar usuario</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estilos.css">
+</head>
+<body>
+    <form method="post">
+    	<h1>Conexion</h1>
+    	<input type="text" name="name" placeholder="Nombre completo">
+    	<input type="number" name="edad" placeholder="Edad">
+    	<input type="submit" name="register">
+    </form>
+        <?php 
+        include("registrarDatos.php");
+        ?>
+</body>
+</html>
