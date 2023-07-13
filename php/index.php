c055fe03b4ca23b54fc77ae0f4dcd9c996c59a28
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-12 20:57:41 -0500

    Update index.php

--- php/index.php
+++ php/index.php
@@ -3,7 +3,7 @@
 <head>
 	<title>Registrar usuario</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href=".css\estilos.css">
+	<link rel="stylesheet" type="text/css" href="estilos.css">
 </head>
 <body>
     <form method="post">
