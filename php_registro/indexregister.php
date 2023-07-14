0a9c46020112d262f9981b9a65ae67541ce5fc2f
Alexander0127 <AlexanderTR0127@gmail.com>
2023-07-13 21:49:55 -0500

    css

--- php_registro/indexregister.php
+++ php_registro/indexregister.php
@@ -6,17 +6,18 @@
 	<link rel="stylesheet" type="text/css" href="estilos_registro.css">
 </head>
 <body>
-	<h1>Registrarse</h1>
-    <form method="post">
-    	
-    	<input type="text" name="name" placeholder="Nombres">
-		<input type="text" name="lastname" placeholder="Apellidos">
-    	<input type="number" name="phone" placeholder="Número Celular">
-		<input type="text" name="addres" placeholder="Dirección de entrega">
-		<input type="text" name="email" placeholder="Correo Electrónico">
-		<input type="text" name="password" placeholder="Contraseña">
-    	<input type="submit" name="register">
-    </form>
+	<div class="formulario">
+		<form method="post">
+			<input type="text" name="name" placeholder="Nombres">
+			<input type="text" name="lastname" placeholder="Apellidos">
+			<input type="number" name="phone" placeholder="Número Celular">
+			<input type="text" name="addres" placeholder="Dirección de entrega">
+			<input type="text" name="email" placeholder="Correo Electrónico">
+			<input type="text" name="password" placeholder="Contraseña">
+			<input type="submit" name="register">
+    	</form>
+	</div>
+    
         <?php 
         include("registerInput.php");
         ?>
