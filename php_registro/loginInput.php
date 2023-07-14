fc162a2ed3788ecc3e18cfc31d72ff6b31b414a8
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-14 12:48:07 -0500

    Update loginInput.php

--- php_registro/loginInput.php
+++ php_registro/loginInput.php
@@ -6,17 +6,17 @@ if (isset($_POST['LOGIN'])) {
 		
 		$email = trim($_POST['email']);
 		$password = trim($_POST['password']);
-        $verificar_id = mysqli_query($conex, "SELECT * FROM register WHERE email='$email' and contraseña = $password");    
-        if(mysqli_num_rows($verificar_id) > 0){
-            if ($resultado) 
-				{header("location: bienvenido.php");
-			} 
-            else {
-				?> 
+        $verificar_login = mysqli_query($conex, "SELECT * FROM register WHERE email='$email' and contrasena = '$password'");    
+        if(mysqli_num_rows($verificar_login) > 0){
+            ?>
+                    <h3 class="ok">¡Te has inscripto correctamente!</h3>
+            <?php
+        }
+        else{
+            ?> 
 				<h3 class="bad">¡Ups ha ocurrido un error!</h3>
 			   <?php
-			}
         }
-    	 }
+    	}
 }
 ?>
