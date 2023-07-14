ed86edd71a50d97c6f01515d4f1be90f61687c6c
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-13 23:02:13 -0500

    Update registerInput.php

--- php_registro/registerInput.php
+++ php_registro/registerInput.php
@@ -9,7 +9,18 @@ if (isset($_POST['register'])) {
 		$addres = trim($_POST['addres']);
 		$email = trim($_POST['email']);
 		$password = trim($_POST['password']);
-	    $consulta = "INSERT INTO register(id_usuario, nombre, apellido, telefono, direccion, email, contraseña) VALUES (1,'$name','$lastname','$phone','$addres','$email','$password')";
+
+		do{
+			$pase = 2;
+			$idusu = rand(100000000, 999999999);
+			$verificar_id = mysqli_query($conex, "SELECT * FROM register_usuario WHERE id_usuario='$idusu'");
+			if (mysqli_num_rows($verificar_id) > 0) {
+				$pase = 1;
+			}
+			else{$pase = 0; echo "Numero de pase es: '$pase'";}
+		}while($pase = 0);
+
+	    $consulta = "INSERT INTO register(id_usuario, nombre, apellido, telefono, direccion, email, contraseña) VALUES ($idusu,'$name','$lastname','$phone','$addres','$email','$password')";
 	    $resultado = mysqli_query($conex,$consulta);
 	    if ($resultado) 
 	    	{?>
