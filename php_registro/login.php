1fa1442487e6119a356c5b59f6bfba41189cbb5c
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-14 12:23:33 -0500

    Update login.php

--- php_registro/login.php
+++ php_registro/login.php
@@ -1,1 +1,21 @@
-asd
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Registrar usuario</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="estilos_registro.css">
+</head>
+<body>
+	<div class="formulario">
+		<form method="post">
+			<input type="text" name="email" placeholder="Correo Electrónico">
+			<input type="text" name="password" placeholder="Contraseña">
+			<input type="submit" name="LOGIN">
+    	</form>
+	</div>
+    
+        <?php 
+        include("registerInput.php");
+        ?>
+</body>
+</html>
