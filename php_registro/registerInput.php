b2b45d81323b21ee9e0b68dce46e5c2e2f6675d1
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-13 22:56:32 -0500

    Update registerInput.php

--- php_registro/registerInput.php
+++ php_registro/registerInput.php
@@ -2,32 +2,15 @@
 include("conexion.php");
 
 if (isset($_POST['register'])) {
-    if (strlen($_POST['name']) >= 1 && strlen($_POST['lastname']) >= 1 && strlen($_POST['phone']) >= 1 && strlen($_POST['addres']) >= 1 && strlen($_POST['email']) >= 1 && strlen($_POST['password']) >= 1 && strlen($_POST['id_usuario']) >= 1) {
-		$id_usuario = trim($_POST['id_usuario']);
+    if (strlen($_POST['name']) >= 1 && strlen($_POST['lastname']) >= 1 && strlen($_POST['phone']) >= 1 && strlen($_POST['addres']) >= 1 && strlen($_POST['email']) >= 1 && strlen($_POST['password']) >= 1 ) {
 		$name = trim($_POST['name']);
 	    $lastname = trim($_POST['lastname']);
 		$phone = trim($_POST['phone']);
 		$addres = trim($_POST['addres']);
 		$email = trim($_POST['email']);
 		$password = trim($_POST['password']);
-		
-		do{
-			$pase = 2;
-			$idusu = rand(100000000, 999999999);
-			$verificar_id = mysqli_query($conex, "SELECT * FROM register_usuario WHERE id_usuario='$idusu'");
-			if (mysqli_num_rows($verificar_id) > 0) {
-				$pase = 1;
-			}
-			else{$pase = 0; echo "Numero de pase es: '$pase'";}
-		}while($pase = 0);
-		
-
 	    $consulta = "INSERT INTO register_usuario(id_usuario, nombre, apellido, telefono, direccion, email, contraseña) VALUES ('$id_usuario','$name','$lastname','$phone','$addres','$email','$password')";
 	    $resultado = mysqli_query($conex,$consulta);
-		
-
-		
-
 	    if ($resultado) 
 	    	{?>
 	    	<h3 class="ok">¡Te has inscripto correctamente!</h3>
