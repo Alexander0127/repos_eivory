9de69815752b423adad25aa23e7012db51c0e067
Alexander0127 <AlexanderTR0127@gmail.com>
2023-07-13 21:20:24 -0500

    erdas

--- php_registro/indexregister.php
+++ php_registro/indexregister.php
@@ -3,16 +3,17 @@
 <head>
 	<title>Registrar usuario</title>
 	<meta charset="utf-8">
-	<link rel="stylesheet" type="text/css" href="estilos.css">
+	<link rel="stylesheet" type="text/css" href="estilos_registro.css">
 </head>
 <body>
+	<h1>Registrarse</h1>
     <form method="post">
-    	<h1>Conexion</h1>
+    	
     	<input type="text" name="name" placeholder="Nombres">
-		<input type="text" name="lastname" placehorder="Apellidos">
+		<input type="text" name="lastname" placeholder="Apellidos">
     	<input type="number" name="phone" placeholder="Número Celular">
 		<input type="text" name="addres" placeholder="Dirección de entrega">
-		<input type="text" name="email" palceholder="Correo Electrónico">
+		<input type="text" name="email" placeholder="Correo Electrónico">
 		<input type="text" name="password" placeholder="Contraseña">
     	<input type="submit" name="register">
     </form>
