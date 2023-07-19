eede6733e404c351e112df562547e030a46fceca
Alexander0127 <AlexanderTR0127@gmail.com>
2023-07-19 16:18:36 -0500

    meta viewport

--- php_registro/indexregister.php
+++ php_registro/indexregister.php
@@ -3,6 +3,7 @@
 <head>
 	<title>Registrar usuario</title>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="../estilos/estilos_registro.css">
 </head>
 <body>
--- php_registro/login.php
+++ php_registro/login.php
@@ -3,6 +3,7 @@
 <head>
 	<title>LOG DE USUARIO</title>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="../estilos/estilos_login.css">
 </head>
 <body class="content">
