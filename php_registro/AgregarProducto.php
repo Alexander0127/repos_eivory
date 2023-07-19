f613e2afae5e69b9e5e091f4c35d8f6ecb45339c
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-18 23:57:52 -0500

    Update AgregarProducto.php

--- php_registro/AgregarProducto.php
+++ php_registro/AgregarProducto.php
@@ -9,7 +9,18 @@
             $precio_producto = trim($_POST['precio']);
             $img_producto = trim($_POST['imgproducto']);
 
-            $verificar_aggProducto = mysqli_query()
+            $verificar_aggProducto = mysqli_query($conex, "INSERT INTO tabla_productos(id_producto, name_producto, precio_producto, img_producto) VALUES ('$codigo_producto','$name_producto','$precio_producto','$img_producto')");
+
+            if ($verificar_aggProducto) 
+	    	{?>
+	    	<h3 class="ok">¡Te has inscripto correctamente!</h3>
+            <?php
+	        } 
+            else {
+	    	?> 
+	    	<h3 class="bad">¡Ups ha ocurrido un error!</h3>
+           <?php
+	        }
         }
     }
 
