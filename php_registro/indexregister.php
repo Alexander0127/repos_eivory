b108283dfca4ae665a5a8e29c8de95457c3af913
Alexander0127 <AlexanderTR0127@gmail.com>
2023-07-18 23:33:32 -0500

    s

--- php_registro/indexregister.php
+++ php_registro/indexregister.php
@@ -13,9 +13,7 @@
 	<main>
 		<div class="formulario">
 			<h2>Crear cuenta</h2>
-			<?php 
-        include("registerInput.php");
-        ?>
+			
 			<form method="post">
 				<input type="text" name="name" placeholder="Nombres">
 				<input type="text" name="lastname" placeholder="Apellidos">
@@ -29,6 +27,8 @@
 		
 	</main>
     
-        
+	<?php 
+        	include("registerInput.php");
+        	?>
 </body>
 </html>
