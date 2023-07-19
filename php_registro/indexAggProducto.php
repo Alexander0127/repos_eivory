4a93a1c997d00e650451b066b3e32f70f4ce4cd8
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-18 23:12:29 -0500

    Create indexAggProducto.php

---
+++ php_registro/indexAggProducto.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+        <div class="formulario">
+			<h2>Crear cuenta</h2>
+			<form method="post">
+				<input type="text" name="nameproducto" placeholder="Nombre del producto">
+				<input type="text" name="codigoproducto" placeholder="codigo producto">
+				<input type="number" name="precio" placeholder="Precio Producto">
+				<input type="file" name="imgproducto" placeholder="Imagen producto">
+				<input type="submit" name="AgregarProducto">
+			</form>
+		</div>
+</body>
+</html>
