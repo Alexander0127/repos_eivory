7d6db548c6b4623fb1a6d4041bf296a2af2ff404
Alexander0127 <AlexanderTR0127@gmail.com>
2023-07-18 23:32:34 -0500

    formulario css

--- php_registro/indexregister.php
+++ php_registro/indexregister.php
@@ -13,6 +13,9 @@
 	<main>
 		<div class="formulario">
 			<h2>Crear cuenta</h2>
+			<?php 
+        include("registerInput.php");
+        ?>
 			<form method="post">
 				<input type="text" name="name" placeholder="Nombres">
 				<input type="text" name="lastname" placeholder="Apellidos">
@@ -20,13 +23,12 @@
 				<input type="text" name="addres" placeholder="Dirección de entrega">
 				<input type="text" name="email" placeholder="Correo Electrónico">
 				<input type="text" name="password" placeholder="Contraseña">
-				<input type="submit" name="register">
+				<input id="form-button-crear" type="submit" value="Registrarse" name="register">
 			</form>
 		</div>
+		
 	</main>
     
-        <?php 
-        include("registerInput.php");
-        ?>
+        
 </body>
 </html>
