fc6132d8b2ab83225f800534eb2b158f6f9acc23
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-19 01:45:25 -0500

    Update indexAggProducto.php

--- php_registro/indexAggProducto.php
+++ php_registro/indexAggProducto.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="estilos_registro.css">
+    <link rel="stylesheet" type="text/css" href="../estilos/estilos_registro.css">
     <title>AGREGAR PRODUCTOS</title>
 </head>
 <body>
