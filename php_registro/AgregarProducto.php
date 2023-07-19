7fef6b670e91d7731944621ede001c831f6fd3a7
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-19 00:56:23 -0500

    Update AgregarProducto.php

--- php_registro/AgregarProducto.php
+++ php_registro/AgregarProducto.php
@@ -7,7 +7,7 @@
             $codigo_producto = trim($_POST['codigoproducto']);
             $name_producto = trim($_POST['nameproducto']);
             $precio_producto = trim($_POST['precio']);
-            $img_producto = addslashes(file_get_contents($_FILES['imgproducto']['temp_img']));
+            $img_producto = addslashes(file_get_contents($_FILES['imgproducto']['tmp_name']));
 
             $agregado = "INSERT INTO tabla_productos(id_producto, name_producto, precio_producto, img_producto) VALUES ('$codigo_producto','$name_producto','$precio_producto','$img_producto')";
             $verificar_aggProducto = mysqli_query($conex,$agregado);
