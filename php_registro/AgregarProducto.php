a95faa20469547e44f544edf95a726ce48cc1992
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-18 23:32:22 -0500

    Create AgregarProducto.php

---
+++ php_registro/AgregarProducto.php
@@ -0,0 +1,8 @@
+<?php
+    include("conexion.php");
+
+    if(isset($_POST['AgregarProducto'])){
+        if(strlen($_POST['']) >= 1 && strlen($_POST['']) >= 1 $$ strlen($_POST['']) >= 1){}
+    }
+
+?>
