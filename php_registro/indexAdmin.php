44e7fbe9c84f77641e45646bf7599e9e5bd968d3
yeremiesx25 <j4r4my_2002@hotmail.com>
2023-07-20 01:29:07 -0500

    aa

--- php_registro/indexAdmin.php
+++ php_registro/indexAdmin.php
@@ -2,10 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="../estilos/estilos_encabezado.css">
+    <title>Index de ADMIN</title>
 </head>
 <body>
-    <h1>PAGINA ADMINISTRADOR</h1>
+    <header> 
+        <div class="logo">
+            <img src="../Imagenes/Lgg.png" alt="Logo Eivory">
+            <h2 class="nombre_empresa">Somos vida</h2>
+        </div>
+        <nav>
+                <a href="" class="nav_link">AGREGAR</a>
+                <a href="" class="nav_link">ELIMINAR</a>
+                <a href="" class="nav_link">MODIFICAR</a>
+        </nav>
+    </header>
 </body>
 </html>
