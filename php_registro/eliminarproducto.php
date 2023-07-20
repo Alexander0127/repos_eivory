b2eb1b30a87dd3f2cf44253fac80ff3ea8755968
PieroNF <pgianmarco10@gmail.com>
2023-07-20 01:32:25 -0500

    a

---
+++ php_registro/eliminarproducto.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="../estilos/estilos_registro.css">
+    <title>PRODUCTOS</title>
+</head>
+<body>
+        <div class="formulario">
+			<h2>PRODUCTO</h2>
+
+			<form action="eliminarproducto.php" method="post" enctype="multipart/form-data">				
+				<input type="text" name="codigoproducto" placeholder="codigo producto">
+				<input id="form-button-eliminar" type="submit" value="Eliminar" name="EliminarProducto">
+			</form>
+		</div>
+        
+        <?php 
+        	include("conexion.php");
+
+            $id = trim($_POST['id']);
+
+            $eliminar = mysqli_query($conex, "DELETE FROM 'tabla_productos' WHERE 'tabla_productos'.'id' = ;");    
+        ?>
+</body>
+</html>
--- php_registro/mostrarproductos.php
+++ php_registro/mostrarproductos.php
@@ -14,7 +14,7 @@
         $muestra = "SELECT name_producto, precio_producto, img_producto FROM tabla_productos";
         $conexion_muestra = mysqli_query($conex, $muestra);
         while($mostrar = mysqli_fetch_array($conexion_muestra)){
-            ?>
+    ?>
             <table border="2">
             <tr><td><img width="200px" src="data:image/jpg;base64, <?php echo base64_encode($mostrar['img_producto'])?>"></td></tr>
             <tr><td><?php echo $mostrar['name_producto']?></td></tr>
