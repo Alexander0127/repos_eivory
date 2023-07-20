4f8116792e30c2edfdf048bbf79d32b74ab39f69
PieroNF <pgianmarco10@gmail.com>
2023-07-20 12:03:54 -0500

    Buscar

--- php_registro/eliminarproducto.php
+++ php_registro/eliminarproducto.php
@@ -4,29 +4,51 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../estilos/estilos_registro.css">
-    <title>PRODUCTOS</title>
+    <title>ELIMINAR</title>
 </head>
 <body>
         <div class="formulario">
-			<h2>PRODUCTO</h2>
+			<h2>ELIMINAR PRODUCTOS</h2>
 
 			<form action="eliminarproducto.php" method="post" enctype="multipart/form-data">				
 				<input type="text" name="codigoproducto" placeholder="codigo producto">
+                <input type="submit" name="busqueda" value="Buscar">
 				<input id="form-button-eliminar" type="submit" value="Eliminar" name="EliminarProducto">
 			</form>
+            
 		</div>
         
         <?php 
         	include("conexion.php");
            
-            if (isset($_POST['EliminarProducto'])) {
-                if (strlen($_POST['codigoproducto']) >= 1)
-                {
+           
+            if (isset($_POST['busqueda'])) {
+                if (strlen($_POST['codigoproducto']) >= 1) {
+                   
                     $id = trim($_POST['codigoproducto']);
-                    $eliminar = mysqli_query($conex, "DELETE FROM tabla_productos WHERE id_producto='$id'");    
+                    $consulta = mysqli_query($conex, "SELECT * FROM tabla_productos WHERE id_producto='$id'");
+        
+                    if (mysqli_num_rows($consulta) === 0) {
+                        echo "Producto no encontrado.";
+                    } else {
+                        $producto = mysqli_fetch_assoc($consulta);
+                        echo "<h2>DETALLES DEL PRODUCTO:</h2>";
+                        echo "Nombre: " . $producto['name_producto'] . "<br>";
+                        echo "Precio: " . $producto['precio_producto'] . "<br>";
+                        echo "Imagen: <br>";
+
+                    }
                 }
             }    
-
-        ?>
+            
+            if (isset($_POST['EliminarProducto'])) {
+                if (strlen($_POST['codigoproducto']) >= 1) {
+                    $id = trim($_POST['codigoproducto']);
+                    $eliminar = mysqli_query($conex, "DELETE FROM tabla_productos WHERE id_producto='$id'");
+                    echo "Producto eliminado correctamente.";
+                }
+            }    
+            ?>
+   
 </body>
 </html>
