cf47066632415293bb8faafa4f6acf09c8b9ecba
Alexander0127 <AlexanderTR0127@gmail.com>
2023-07-20 19:55:29 -0500

    sdsdds

--- php_registro/conexion.php
+++ php_registro/conexion.php
@@ -1,5 +1,5 @@
 <?php
     #variable para conectar con la base de datos
-    #tiene 4 parámetros
+    #tiene 4 parámetros (nombre del host, usuario, contraseña, nombre db)
     $conex = mysqli_connect("db4free.net:3306","alexandertr0127","shaggypieroyeremiesalexander","z_aguaeivory");
 ?>
